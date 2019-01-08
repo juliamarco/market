0dd233de8e821c6e51a400d74984a0e10719b8c4
Julia Marco <marcootj@gmail.com>
2019-01-08 09:20:09 -0700

    Added test_it_has_a_name{

--- test/market_test.rb
+++ test/market_test.rb
@@ -8,8 +8,14 @@ class MarketTest < Minitest::Test
 
   def test_it_exists
 
-    market = Market.new
+    market = Market.new("South Pearl Street Farmers Market")
     assert_instance_of Market, market
+  end
+
+  def test_it_has_a_name
+
+    market = Market.new("South Pearl Street Farmers Market")
+    assert_equal "South Pearl Street Farmers Market", market.name
   end
 
 end
